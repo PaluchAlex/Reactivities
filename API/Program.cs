3267aa410004e59350aecc9025aa949822a2bdfe
Paluch Alex-Sebastian <alexpaluch43@gmail.com>
2024-02-18 23:55:20 +0200

    Creating the database

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<DataContext>(opt => {
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -28,5 +29,21 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An err ocurred during migration");
+    throw;
+}
 
 app.Run();
